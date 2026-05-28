82c7f407c05dd80f85200f668cf393880899f7b5
jaysecotools <jayrowley72@hotmail.com>
2026-05-28 15:43:29 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,33 @@
 const CACHE = "tasveg-v7.2";
 
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./storage.js",
-  "./export.js",
-  "./manifest.json",
-  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
-  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    "./",
+    "./index.html",
+    "./app.js",
+    "./storage.js",
+    "./export.js",
+    "./manifest.json",
+    "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
+    "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
 ];
 
 self.addEventListener("install", e => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    );
 });
 
 self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
+    e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request).catch(() => {
+                return caches.match("./index.html");
+            });
+        })
+    );
 });
